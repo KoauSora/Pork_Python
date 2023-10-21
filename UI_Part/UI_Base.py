d34c332d81f81919f0b92c694bf7c47315cbbc18
Sora <142004515+KoauSora@users.noreply.github.com>
2023-10-21 22:13:29 +0800

    一点修改和注释

--- UI_Part/UI_Base.py
+++ UI_Part/UI_Base.py
@@ -5,20 +5,40 @@ from abc import ABC, abstractmethod
 class UI_Base(ABC):
     @abstractmethod
     def screen_shot(self):
+        """
+        这个函数用来截取屏幕图像
+        :return: None
+        """
         pass
 
     @abstractmethod
     def get_mat(self):
+        """
+        支持多线程的获取图片函数
+        :return: cv.image
+        """
         pass
 
     @abstractmethod
     def need_more_thread(self):
+        """
+        当list中存在内容时，进行多线程并行运算
+        :return:
+        """
         pass
 
     @abstractmethod
     def get_ready(self):
+        """
+        表示可以开始截屏的函数，主要提供给前端调用
+        :return:
+        """
         pass
 
     @abstractmethod
     def not_ready(self):
+        """
+        无需截屏，提供给前端使用
+        :return:
+        """
         pass
--- main.py
+++ main.py
@@ -1,30 +1,19 @@
 import time
-
-# modified by XuEddie
-
 import cv2 as cv
 import torch
-
 import threading
 import concurrent.futures
-
 from UI_Part.Ui import UI
 from UI_Part.UI_Widgets import MyApp
 
-# rewrite by yuanshen
-
-
 complet_result = []
-
 event3 = threading.Event()
 Run_or_Not = False
 
 
 def function1():
     # 这里用来处理图像并且输出处理的结果,以下是测试程序
-
     event3.wait()
-    time.sleep(10)
     print("开始执行线程1！")
 
     # 这里是添加分析函数的地方my_task中执行需要分析的函数，提供了image的接口，这将返回一张图片，请使用此变量进行操作
@@ -61,12 +50,10 @@ def function2():
 def function3():
     # UI界面
     widgets = MyApp(400, 300)
-
     global Run_or_Not
     Run_or_Not = True
     print("开始执行")
     event3.set()
-
     widgets.run()
     Run_or_Not = False
 
@@ -84,3 +71,5 @@ def main():
     thread2.join()
     thread3.join()
 
+
+main()
